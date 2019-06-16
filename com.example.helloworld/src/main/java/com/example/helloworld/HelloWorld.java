d9cd94340500a500a12196302b1887b0cf90c892
ugurcetin <ugurcetin@ugurcetin>
2019-06-16 13:51:39 +0300

    The First Commit By Ugur

---
+++ com.example.helloworld/src/main/java/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.example.helloworld;
+
+public class HelloWorld {
+	public static void main(String[] helloworld) {
+		var message ="Hello Java 12 For Test Git";
+		System.out.println(message);
+	}
+}
