8223b00b50085063b6640b0f082118ef6f79753c
ugurcetin <ugurcetin@ugurcetin>
2019-06-17 21:26:27 +0300

    put space to end of string

--- com.example.helloworld/src/main/java/com/example/helloworld/HelloWorld.java
+++ com.example.helloworld/src/main/java/com/example/helloworld/HelloWorld.java
@@ -2,7 +2,7 @@ package com.example.helloworld;
 
 public class HelloWorld {
 	public static void main(String[] helloworld) {
-		var message ="Hello Java 12 For Test Git";
+		var message ="Hello Java 12 For Test Git  ";
 		System.out.println(message);
 	}
 }
